9b56ba8c3ecf05d3c15a40aea73f1c9c9ec5aca1
celine <web2@A10.local>
2014-03-25 15:27:20 +0100

    portfolio2
    version2

--- portfolio/wp-content/themes/reverie-master/footer.php
+++ portfolio/wp-content/themes/reverie-master/footer.php
@@ -9,13 +9,32 @@
 
 <footer class="full-width" role="contentinfo">
 	<div class="row">
-		<div class="large-12 columns">
+		<div class="large-5 columns">
+			<form action="" method="post">
+			    <input type="text" name="user_email" id="user_email" placeholder="Votre email" class="" />
+			    <input type="text" name="user_name" id="user_name" placeholder="Nom" />
+			    <input type="text" name="user_fname" id="user_fname" placeholder="Prénom" />
+			    <textarea name="textMessage" placeholder="Ecrivez votre message ici ..."></textarea>
+			    <input type="submit" name="submitPost" value="Envoyer"  />
+			</form>
+		</div>
+		<div class="large-3 columns">
 			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
+		</div>
+		<div class="large-4 columns">
+			<h3>Suivez-moi sur le web</h3>
+			<ul class="top-bar">
+		    	<li class="reseau">
+					<a href="" rel="linkedIn"> <img src="http://dev.tisseur-de-toile.fr/mjm-web2/celine/portfolio/wp-content/uploads/2014/03/linkedin.png" class="sociaux" alt="linkedin"> </a>
+	        		<a href="" rel="google+"> <img src="http://dev.tisseur-de-toile.fr/mjm-web2/celine/portfolio/wp-content/uploads/2014/03/google+.png" class="sociaux" alt="google+"> </a>
+	        		<a href="" rel="twitter"> <img src="http://dev.tisseur-de-toile.fr/mjm-web2/celine/portfolio/wp-content/uploads/2014/03/linkedin.png" class="sociaux" alt="twitter"> </a>
+	        	</li>
+		    </ul>
 		</div>
 	</div>
 	<div class="row love-reverie">
 		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
 		</div>
 	</div>
 </footer>
